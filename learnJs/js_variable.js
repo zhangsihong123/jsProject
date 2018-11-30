a0cf3d70739a74286cfb7b5f25795e45c8a6587b
zhangsihong123 <azsh19941014>
2018-11-30 18:11:21 +0800

    学习JavaScript到变量

---
+++ learnJs/js_variable.js
@@ -0,0 +1,27 @@
+// 变量是存储信息的容器
+
+//1、实例：
+ 
+var x = 2;
+var y = 5;
+var z = x + y
+
+document.write(x+"<br>");
+document.write(y+"<br>");
+document.write(z+"<br>");
+
+/**
+ * 当您向变量分配文本值时，应该用双引号或单引号包围这个值。
+ * 当您向变量赋的值是数值时，不要使用引号。如果您用引号包围数值，该值会被作为文本来处理。
+ * 
+ * 由于JavaScript是弱类型所以变量的类型由最后赋的值决定
+ */
+var pi=3.14;            //数字 int类型
+var name="Bill Gates";  //字符串 string类型
+var answer='Yes I am!'; //字符串 string类型
+
+//声明(创建)JavaScript变量 使用var
+var carname = 'Vivo';//可以用双引号"",也可以用单引号''
+
+
+
---
+++ learnJs/myTestJs.js
@@ -0,0 +1,3 @@
+function doClick(){
+    document.getElementById("demo").innerHTML="我的第一个web页面"
+}
